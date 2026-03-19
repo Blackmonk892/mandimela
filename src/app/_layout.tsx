b22cc57d4239e2308d5b2c168f88f8a2e1ac84cc
Blackmonk892 <blackmonk2224@gmail.com>
2026-03-19 14:11:30 +0530

    made the sign-in ui , auth works now

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,12 @@
 import { ClerkProvider } from "@clerk/expo";
 import { tokenCache } from "@clerk/expo/token-cache";
-
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
 import "../../global.css";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
@@ -10,9 +15,12 @@ if (!publishableKey) {
   throw new Error("Add your Clerk Publishable key to the .env file");
 }
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <Stack screenOptions={{ headerShown: false }} />
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </ThemeProvider>
     </ClerkProvider>
   );
 }
